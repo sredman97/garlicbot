b8bbc43594808616b444aa75235c30ab29e9da9f
sredman97 <saredman.97@gmail.com>
2018-03-09 02:24:39 -0600

    fixed emoji and added gbool for test

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import json
 import requests
 
 client = discord.Client()
+test = True
 
 @client.event
 async def on_ready():
@@ -25,20 +26,26 @@ def fetch_garlic(to_get):
 async def fetch_change_wait():
     while True:
         price = fetch_garlic('price_usd')
-        to_change = client.get_channel('381265965985562634')
+        if test:
+            to_change = client.get_channel('421514445815021588')
+        else:
+            to_change = client.get_channel('381265965985562634')
         await client.edit_channel(to_change, name=price + " USD")
         await asyncio.sleep(30)
             
 @client.event
 async def on_message(message):
-    if message.content.contains('garlic'):
-        await client.add_reaction(message, ':garlic:')
+    if message.author.bot:
+        return;
+    if 'garlic' in message.content:
+        emoji = discord.utils.get(client.get_all_emojis(), name='garlic')
+        await client.add_reaction(message, emoji)
     elif message.content.startswith('!help'):
         await client.send_message(message.channel, \
-                '!rank - display rank of garlicoin\n \
-                !pbtc - price in bitcoin\n \
-                !pchg - percent change in last 1h\n \
-                !pc24 - percent change in last 24h\n')
+            ' !rank - display rank of garlicoin\n \
+            !pbtc - price in bitcoin\n \
+            !pchg - percent change in last 1h\n \
+            !pc24 - percent change in last 24h\n')
     elif message.content.startswith('!rank'):
         rank = fetch_garlic('rank')
         await client.send_message(message.channel, rank)
@@ -51,6 +58,9 @@ async def on_message(message):
     elif message.content.startswith('!pc24'):
         pc24 = fetch_garlic('percent_change_24h')
         await client.send_message(message.channel, pc24)
-    
-client.run('NDIxNTUzMjQyOTA0NzIzNDcx.DYO5kQ.-HGJ3Va2g0BEFozBD5BcsZyz-m8')
+
+if test:
+    client.run('NDIxNTE0MTE3MDQwNDM5MzA3.DYPLeA.9im1jZIL5ZR59WIXoJ4NS0moWm8')
+else:
+    client.run('NDIxNTUzMjQyOTA0NzIzNDcx.DYO5kQ.-HGJ3Va2g0BEFozBD5BcsZyz-m8')
         
