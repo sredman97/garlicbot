527ad07f84bc954b6203fa316fd50fde05b55f1f
sredman97 <saredman.97@gmail.com>
2018-03-09 22:57:33 -0600

    added random meme getting and adding functionality as well as csv file to keep all memes

--- meme.py
+++ meme.py
@@ -1,6 +1,10 @@
 import discord
 from discord.ext import commands
 
+import random
+import sys
+import csv
+
 class Meme():
 
     def __init__(self, bot):
@@ -10,6 +14,29 @@ class Meme():
     async def hepl(self, ctx):
         """gives hepl"""
         await self.bot.send_message(ctx.message.channel, 'https://twitter.com/Inky_Cap/status/947582060922781696')
+
+    @commands.command(pass_context=True)
+    async def getmeme(self, ctx):
+        """retrieves meme for your enjoyment"""
+        with open('bigbook.csv') as f:
+            memes = csv.reader(f)
+            meme = random.choice(list(memes))
+        await self.bot.send_message(ctx.message.channel, meme[0])
+
+    @commands.command(pass_context=True)
+    async def addmeme(self, ctx, arg=None):
+        """add meme for future enjoyment"""
+        if arg is None:
+            await self.bot.send_message(ctx.message.channel, 'Submit youtube link \n usage: !addmeme youtubelink')
+            return;
+        elif 'https://www.youtube.com/watch?' not in arg:
+            await self.bot.send_message(ctx.message.channel, 'Must be a valid youtube link')
+            return;
+        with open('bigbook.csv', 'a') as f:
+            memes = csv.writer(f)
+            towrite = [arg]
+            print('writing {} to bigbook'.format(arg))
+            memes.writerow(towrite)
 
 def setup(bot):
     bot.add_cog(Meme(bot))
