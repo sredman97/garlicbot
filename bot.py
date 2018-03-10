ad700b9209ec1f683c769bd735d2f1f6fbcccad3
sredman97 <saredman.97@gmail.com>
2018-03-09 20:53:09 -0600

    modified to use cogs instead of on_message

--- bot.py
+++ bot.py
@@ -1,23 +1,27 @@
 #by Shane :)
 
 import discord
+from discord.ext import commands
 import asyncio
 import json
 import requests
 
-client = discord.Client()
+
+startup_extensions = ["garlic"]
+
+bot = commands.Bot(command_prefix='!')
 test = True
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
-    client.loop.create_task(fetch_change_wait())
+    bot.loop.create_task(fetch_change_wait())
     return;
 
-def fetch_garlic(to_get):
+async def fetch_garlic(to_get):
     r = requests.get('https://api.coinmarketcap.com/v1/ticker/garlicoin')
     for coin in r.json():
         got = coin.get(to_get)
@@ -25,42 +29,33 @@ def fetch_garlic(to_get):
     
 async def fetch_change_wait():
     while True:
-        price = fetch_garlic('price_usd')
+        price = await fetch_garlic('price_usd')
         if test:
-            to_change = client.get_channel('421514445815021588')
+            to_change = bot.get_channel('421514445815021588')
         else:
-            to_change = client.get_channel('381265965985562634')
-        await client.edit_channel(to_change, name=price + " USD")
+            to_change = bot.get_channel('381265965985562634')
+        await bot.edit_channel(to_change, name=price + " USD")
         await asyncio.sleep(30)
             
-@client.event
+@bot.event
 async def on_message(message):
     if message.author.bot:
         return;
     if 'garlic' in message.content:
-        emoji = discord.utils.get(client.get_all_emojis(), name='garlic')
-        await client.add_reaction(message, emoji)
-    elif message.content.startswith('!help'):
-        await client.send_message(message.channel, \
-            ' !rank - display rank of garlicoin\n \
-            !pbtc - price in bitcoin\n \
-            !pchg - percent change in last 1h\n \
-            !pc24 - percent change in last 24h\n')
-    elif message.content.startswith('!rank'):
-        rank = fetch_garlic('rank')
-        await client.send_message(message.channel, rank)
-    elif message.content.startswith('!pbtc'):
-        pbtc = fetch_garlic('price_btc')
-        await client.send_message(message.channel, pbtc)
-    elif message.content.startswith('!pchg'):
-        pchg = fetch_garlic('percent_change_1h')
-        await client.send_message(message.channel, pchg)
-    elif message.content.startswith('!pc24'):
-        pc24 = fetch_garlic('percent_change_24h')
-        await client.send_message(message.channel, pc24)
+        emoji = discord.utils.get(bot.get_all_emojis(), name='garlic')
+        await bot.add_reaction(message, emoji)
+    await bot.process_commands(message)
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, e))
 
 if test:
-    client.run('NDIxNTE0MTE3MDQwNDM5MzA3.DYPLeA.9im1jZIL5ZR59WIXoJ4NS0moWm8')
+    bot.run('NDIxNTE0MTE3MDQwNDM5MzA3.DYPLeA.9im1jZIL5ZR59WIXoJ4NS0moWm8')
 else:
-    client.run('NDIxNTUzMjQyOTA0NzIzNDcx.DYO5kQ.-HGJ3Va2g0BEFozBD5BcsZyz-m8')
+    bot.run('NDIxNTUzMjQyOTA0NzIzNDcx.DYO5kQ.-HGJ3Va2g0BEFozBD5BcsZyz-m8')
         
